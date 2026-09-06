04c60fc13ad1bf230187e3aac1fff2e5bbd8b34d
Adammohamed-1 <adamaamohamed@gmail.com>
2026-09-06 19:26:47 +0100

    feat: add EKS control plane

---
+++ terraform/eks.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "eks_cluster" {
+  name = "platform-ops-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "main" {
+  name     = "platform-ops-eks"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public_a.id,
+      aws_subnet.public_b.id
+    ]
+
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name = "platform-ops-eks"
+  }
+}
